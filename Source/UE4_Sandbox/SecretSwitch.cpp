23f87dab02efe07450788d9b5aed360776948441
cnpatton <botanicality@gmail.com>
2019-08-03 13:23:53 -0500

    Added a switch trigger and added logging for PoC testing

--- Source/UE4_Sandbox/SecretSwitch.cpp
+++ Source/UE4_Sandbox/SecretSwitch.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SecretSwitch.h"
-
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 ASecretSwitch::ASecretSwitch()
@@ -9,6 +10,22 @@ ASecretSwitch::ASecretSwitch()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	RootComponent = TriggerBox;
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	// Ignore all collision
+	TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+	// Only allow overlap collision from pawn
+	TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	TriggerBox->SetBoxExtent(FVector(62.f, 62.f, 32.f));
+
+	PhysicalSwitch = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FloorSwitch"));
+	PhysicalSwitch->SetupAttachment(GetRootComponent());
+
+	ActivatedTarget = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ActivatedTarget"));
+	ActivatedTarget->SetupAttachment(GetRootComponent());
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +33,9 @@ void ASecretSwitch::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Overlap delegates setup
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &ASecretSwitch::OnTriggerActivated);
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &ASecretSwitch::OnTriggerDeactivated);
 }
 
 // Called every frame
@@ -23,5 +43,15 @@ void ASecretSwitch::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ASecretSwitch::OnTriggerActivated(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Trigger Activated..."));
+}
+
+void ASecretSwitch::OnTriggerDeactivated(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndext)
+{
+	UE_LOG(LogTemp, Warning, TEXT("...Trigger Deactivated"));
 }
 
--- Source/UE4_Sandbox/SecretSwitch.h
+++ Source/UE4_Sandbox/SecretSwitch.h
@@ -15,6 +15,18 @@ public:
 	// Sets default values for this actor's properties
 	ASecretSwitch();
 
+	// Logical area for which player can overlap with
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = SecretSwitch)
+	class UBoxComponent* TriggerBox;
+
+	// visual area for the switch
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = SecretSwitch)
+	UStaticMeshComponent* PhysicalSwitch;
+
+	// Thing that gets "activated" upon trigger overlap
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = SecretSwitch)
+	UStaticMeshComponent* ActivatedTarget;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,6 +35,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+	UFUNCTION()	// must be a UFUNCTION to act as a delegate
+	void OnTriggerActivated(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult);
+
+	UFUNCTION()	// must be a UFUNCTION to act as a delegate
+	void OnTriggerDeactivated(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndext);
 	
 };
